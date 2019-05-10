28a10ca4af79f32548a72be60cf518caa7e5c16f
ethankwok <guoyangquan@gs-robot.com>
2019-05-10 14:46:34 +0800

    gauss newton front end.

---
+++ src/gn_frontEnd.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ethan on 19-5-10.
+//
+
+#include "gn_frontEnd.h"
---
+++ src/gn_frontEnd.h
@@ -0,0 +1,8 @@
+//
+// Created by ethan on 19-5-10.
+//
+
+#ifndef ETHAN_LSLAM_GN_FRONTEND_H
+#define ETHAN_LSLAM_GN_FRONTEND_H
+
+#endif //ETHAN_LSLAM_GN_FRONTEND_H
